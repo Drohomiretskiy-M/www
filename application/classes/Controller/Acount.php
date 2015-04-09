64e996ef10cf4f6d9e76149fb4f6f8fcfb22156c
Misha <drohomiretskiy@gmail.com>
2015-04-09 13:54:00 +0300

    сесії
    сесія е працює

--- application/classes/Controller/Acount.php
+++ application/classes/Controller/Acount.php
@@ -6,8 +6,8 @@ class Controller_Acount extends Controller_Common {
       public function before()
 
     {
-     // $session = Session::instance();
-     // $session->set('auth_redirect', $_SERVER['REQUEST_URI']);
+     $session = Session::instance();
+     $session->set('auth_redirect', $_SERVER['REQUEST_URI']);
       $auth = Auth::instance();
       if($auth->logged_in() ==0)
       {
